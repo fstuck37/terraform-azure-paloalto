e06a5fbe937b6bf226867ef0e38396620ff8a822
Stuck <stuckf@dnb.com>
2021-10-28 09:23:28 -0400

    init

--- azurerm_lb.tf
+++ azurerm_lb.tf
@@ -28,7 +28,9 @@ resource "azurerm_lb_probe" "fw_internal_lb_probe" {
 }
 
 resource "azurerm_network_interface_backend_address_pool_association" "interfaces" {
-  for_each = { for s in local.outbound_source_subnets: "${local.firewall_interfaces[s].firewall_name}-${local.firewall_interfaces[s].interface_name}"=>local.firewall_interfaces }
+  for_each = {for interface in local.firewall_interfaces: "${interface.firewall_name}-${interface.interface_name}"=>interface 
+    if interface.subnet_short_name != var.public_subnet_name && interface.subnet_short_name != var.management_subnet_name
+  }
     network_interface_id    = azurerm_network_interface.interfaces["${each.value.firewall_name}-${each.value.interface_name}"].id
     ip_configuration_name   = "${each.value.firewall_name}-${each.value.interface_name}"
     backend_address_pool_id = azurerm_lb_backend_address_pool.outbound_pools[each.value.subnet_short_name].id
